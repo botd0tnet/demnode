99177c127bbced8af59f7e9a5c55c3a9a8837f5f
botd0tnet <74009258+botd0tnet@users.noreply.github.com>
2021-03-13 07:35:14 +0000

    Update dem.sh

--- dem.sh
+++ dem.sh
@@ -6,15 +6,12 @@ for(( i=1; i <=50; i++ ))
 do
 	mix='nym-mixnode'
 	nymmixnode=${mix}${i}
-	if [[ "$(/usr/sbin/service ${nymmixnode} status)" =~ "start/running" ]]
+	if [[ ! "$(/usr/sbin/service ${nymmixnode} status)" =~ "inactive" ]]
 	then
-		node[i]=i
+		node[i-1]=i
 		dem++
 	fi
 done
 
 printf "%b\n\n\n" "${WHITE} Tong so node dang chay: ${YELLOW} ${dem} ${WHITE}"
-printf "%b\n\n\n"
-sleep 2 
 printf "%b\n\n\n" "${WHITE} Cac node dang chay: ${YELLOW} ${node[xx]} ${WHITE}"
-sleep 10
